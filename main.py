9e2e79430b52d96499cbd1c611121e95e0fa95ab
Tammam Bin Masum <tammamxyz@gmail.com>
2025-05-19 16:03:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
 # sup this is my first python program
-print("i like chicken pizza")
-print("it's really good")
-print("yah it's really REALLY REALLY!!! good")
+print("i like pizza pizza good pizza yum :D")
