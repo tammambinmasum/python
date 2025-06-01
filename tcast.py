98ad55bb4a73964e23e49284d9a8c79b98363717
Tammam Bin Masum <tammamxyz@gmail.com>
2025-06-01 22:19:24 +0600

    Update tcast.py

--- tcast.py
+++ tcast.py
@@ -1,7 +1,14 @@
 name = "tammamxyz"
-age = "19"
+age = "placeholder"
 gpa = 1.9
 student = True
 
 age = int(age)
 
+uare = ""
+
+if student == True:
+  uare = "You are a student."
+else:
+  uare = "You are not a student."
+print(f"Your name is {name}. Your age is {age}. Your gpa is {gpa}. {uare}")
